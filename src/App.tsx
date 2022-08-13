f12babe66211f64bc964225c518c366e9d965323
chadlim <chadlimjinjie@gmail.com>
2022-08-13 14:44:10 +0800

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import {
+  ChakraProvider,
+  theme,
+} from "@chakra-ui/react"
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./Home";
+import YouTubeConverter from "./YouTubeConverter";
+
+export default function App({ }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="yt-converter" element={<YouTubeConverter />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  )
+}
