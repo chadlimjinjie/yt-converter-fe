322614801a323d9ecf1b361b4075ae970bc5beb6
chadlim <chadlimjinjie@gmail.com>
2022-08-17 22:12:23 +0800

    Removed unused imports and misc

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from "@chakra-ui/react"
+import { Container, Grid, GridItem } from "@chakra-ui/react"
 import { Outlet } from "react-router-dom"
 import { NavBar } from "../shared/NavBar"
 
@@ -17,7 +17,6 @@ export const DashboardLayout = ({ }) => {
             gridTemplateColumns={'1fr'}
             h='100vh'
             gap='1'
-            // color='blackAlpha.700'
             fontWeight='bold'
         >
             <GridItem area={'header'} p={2}>
@@ -27,11 +26,7 @@ export const DashboardLayout = ({ }) => {
                 <Outlet />
             </GridItem>
             <GridItem area={'footer'} p={2}>
-
-                <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="chadlimjinjie" data-version="v1">
-                    {/* <a className="badge-base__link LI-simple-link" href="https://sg.linkedin.com/in/chadlimjinjie?trk=profile-badge">Chad Lim</a> */}
-                </div>
-
+                <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="chadlimjinjie" data-version="v1"></div>
             </GridItem>
 
         </Grid>
