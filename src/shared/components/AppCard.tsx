9bf897e96b28d0cf207a6c79253eafed8b44d6fe
chadlim <chadlimjinjie@gmail.com>
2022-08-17 22:11:50 +0800

    Refactor AppCard

--- src/shared/components/AppCard.tsx
+++ src/shared/components/AppCard.tsx
@@ -1,21 +1,25 @@
-import { Badge, Box } from "@chakra-ui/react"
-import { StarIcon } from "@chakra-ui/icons"
+import { Badge, Box, Link } from "@chakra-ui/react"
+import {
+    Link as RouterLink
+} from "react-router-dom";
 
-export const AppCard = ({ ...props }) => {
+export const AppCard: React.FC<{ title: string, to?: string }> = ({ title, to = "#" }) => {
 
     return (
-        <Box width='300px' height='300px' borderWidth='1px' borderRadius='lg' p={2}>
-            <Box p='6'>
-                <Box
-                    mt='1'
-                    fontWeight='semibold'
-                    as='h4'
-                    lineHeight='tight'
-                    noOfLines={1}
-                >
-                    {props.title}
+        <Link as={RouterLink} to={to}>
+            <Box width='300px' height='300px' borderWidth='1px' borderRadius='lg'>
+                <Box p='6'>
+                    <Box
+                        mt='1'
+                        fontWeight='semibold'
+                        as='h4'
+                        lineHeight='tight'
+                        noOfLines={1}
+                    >
+                        {title}
+                    </Box>
                 </Box>
             </Box>
-        </Box>
+        </Link>
     )
 }
