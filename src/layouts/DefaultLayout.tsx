de88fd346fb7718f4a6b66bc3b3f1961ddaddd21
chadlim <chadlimjinjie@gmail.com>
2022-08-24 16:18:49 +0800

    removed linkedin badge from layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -25,10 +25,6 @@ export const DefaultLayout = () => {
             </GridItem>
             <GridItem area={'footer'} p={2}>
 
-                <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="chadlimjinjie" data-version="v1">
-                    {/* <a className="badge-base__link LI-simple-link" href="https://sg.linkedin.com/in/chadlimjinjie?trk=profile-badge">Chad Lim</a> */}
-                </div>
-
             </GridItem>
 
         </Grid>
